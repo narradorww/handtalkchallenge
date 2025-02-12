19e52fa6dff8acd74a064b09e5b1b81d3f549f12
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2025-02-12 12:38:45 -0300

    chore: initial commit

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,9 +1,8 @@
-// tests/App.test.tsx
 import React from 'react'
 
 import { render, screen } from '@testing-library/react-native'
 
-import App from './App'
+import App from '../App'
 
 describe('App Component', () => {
   it('should render the "Hello,  World!" text', () => {
